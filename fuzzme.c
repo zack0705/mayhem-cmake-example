d05761ff5f37136b958f10c4b55f6703ccf8bf5d
zack0705 <prajapatizeel75@gmail.com>
2022-04-23 19:02:00 -0400

    convert to libFuzzer target

--- fuzzme.c
+++ fuzzme.c
@@ -7,7 +7,7 @@ int fuzzme(char *buf)
 {
   if(strlen(buf) >= 3)
     if(buf[0] == 'b')
-      if(buf[1] == 'u')
+if(buf[1] == 'u')
         if(buf[2] == 'g') {
           printf("You've got it!");
           abort();
@@ -17,28 +17,8 @@ int fuzzme(char *buf)
 
 #define BUFSZ 256
 
-int main(int argc, char** argv)
+int LLVMFuzzerTestOneInput(char* data, size_t size)
 {
-    char buf[BUFSZ] = { 0 };
-    FILE* f = NULL;
-    size_t nr = 0;
-
-    if (2 > argc) {
-        fprintf(stderr, "usage: %s PAYLOAD\n", argv[0]);
-        return 1;
-    }
-
-    f = fopen(argv[1], "rb");
-    assert(f);
-
-    nr = fread(buf, sizeof(buf[0]), BUFSZ, f);
-    assert(nr > 0);
-    buf[BUFSZ-1] = '\0';
-
-    fuzzme(buf);
-
-    fclose(f);
-
-    return 0;
+  (fuzzme(data));
+  return 0;
 }
-
